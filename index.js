23011690601786faff3ba8db670fc923d931dd24
Nicolasurazanp <nicolasurazanp@gmail.com>
2025-03-07 13:44:17 -0500

    creando el servidor en express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola mundo desde Devtree")
+const express = require('express')
+
+const app =  express()
+
+// Routing
+app.get('/', (req,res) =>{
+   res.send('Hola Mundo en express')
+})
+
+app.get('/ecommerce', (req,res) =>{
+    res.send('Este es el ecommerce')
+ })
+
+
+app.listen(4000, () => {
+    console.log('servidor funcionando')
+    })
